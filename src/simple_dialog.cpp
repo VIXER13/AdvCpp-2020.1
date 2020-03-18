84599f2d8d59fec9b22a89a227d5611c9a279816
Andrey Sokolov <Zokolof@yandex.ru>
2020-03-18 23:19:25 +0300

    Заменил switch на if/else if

--- src/simple_dialog.cpp
+++ src/simple_dialog.cpp
@@ -17,64 +17,47 @@ bool simpleDialog(Process& proc) {
     std::cin >> choice;
     std::array<char, 256> str = {};
 
-    switch (choice) {
-        case 0:
-            return false;
-
-        case 1: {
-            std::cout << "Write string:";
-            std::cin >> str.data();
-            size_t len_write = proc.write(str.data(), strlen(str.data()));
-            if (len_write == std::numeric_limits<size_t>::max()) {
-                std::cerr << "Write error" << std::endl;
-            } else {
-                std::cout << "len_write = " << len_write << std::endl;
-            }
+    if (choice == 0) {
+        return false;
+    } else if (choice == 1) {
+        std::cout << "Write string:";
+        std::cin >> str.data();
+        size_t len_write = proc.write(str.data(), strlen(str.data()));
+        if (len_write == std::numeric_limits<size_t>::max()) {
+            std::cerr << "Write error" << std::endl;
+        } else {
+            std::cout << "len_write = " << len_write << std::endl;
         }
-        break;
-
-        case 2: {
-            if (proc.isReadable()) {
-                size_t len = 0;
-                std::cout << "Read len:";
-                std::cin >> len;
-                size_t len_read = proc.read(str.data(), len);
-                std::cout << "len_read:" << len_read << std::endl
-                          << "String readed:" << str.data() << std::endl;
-            } else {
-                std::cerr << "Not readable" << std::endl;
-            }
+    } else if (choice == 2) {
+        if (proc.isReadable()) {
+            size_t len = 0;
+            std::cout << "Read len:";
+            std::cin >> len;
+            size_t len_read = proc.read(str.data(), len);
+            std::cout << "len_read:" << len_read << std::endl
+                      << "String readed:" << str.data() << std::endl;
+        } else {
+            std::cerr << "Not readable" << std::endl;
         }
-        break;
-
-        case 3: {
-            std::cout << "Write string:";
-            std::cin >> str.data();
-            proc.writeExact(str.data(), strlen(str.data()));
-            std::cout << "writeExact complete!" << std::endl;
+    } else if (choice == 3) {
+        std::cout << "Write string:";
+        std::cin >> str.data();
+        proc.writeExact(str.data(), strlen(str.data()));
+        std::cout << "writeExact complete!" << std::endl;
+    } else if (choice == 4) {
+        if (proc.isReadable()) {
+            size_t len = 0;
+            std::cout << "Read len:";
+            std::cin >> len;
+            proc.readExact(str.data(), len);
+            std::cout << "String readed:" <<  str.data() << std::endl;
+        } else {
+            std::cerr << "Not readable" << std::endl;
         }
-        break;
-
-        case 4: {
-            if (proc.isReadable()) {
-                size_t len = 0;
-                std::cout << "Read len:";
-                std::cin >> len;
-                proc.readExact(str.data(), len);
-                std::cout << "String readed:" <<  str.data() << std::endl;
-            } else {
-                std::cerr << "Not readable" << std::endl;
-            }
-        }
-        break;
-
-        case 5:
-            proc.close();
-        break;
-
-        default:
-            std::cerr << "Command not supported" << std::endl;
-        break;
+    } else if (choice == 5) {
+        proc.close();
+    } else {
+        std::cerr << "Command not supported" << std::endl;
     }
     return true;
 }
